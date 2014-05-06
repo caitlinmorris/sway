d7848c80deeef1480978bf02cdbb330af4d02017
Caitlin Morris <caitlinannemorris@gmail.com>
2014-05-06 15:19:24 +0200

    merged version of non-manual multiplexing with p5 communication

--- multiplex_individually_addressed/multiplex_individually_addressed.ino
+++ multiplex_individually_addressed/multiplex_individually_addressed.ino
@@ -3,39 +3,49 @@
 // Moritz Kemper, IAD Physical Computing Lab
 // ZHdK, 10/12/2011
 
-#define S1 2 // Selection Pins
-#define S2 3
-#define S3 4
+#define analogInput 0 // Common Output (Z)
 
-#define AnalogInput 0 // Common Output (Z)
-
-int analogValue = 0; // Analog Value to read
+int analog1 = 0; // Analog Value to read
 int analog2 = 0;
+
+const int select[] = {2,3,4}; // array of the pins connected to the 4051 input
 
 void setup()
 {
+  
   Serial.begin(9600); // Setup Serial Port
-  pinMode(S1, OUTPUT); // Define Selection Pins as Output
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
+  
+  for(int bit = 0; bit < 3; bit++){
+    pinMode(select[bit], OUTPUT); // set the three select pins to output
+  }
+  
 }
 
 void loop()
 {
-  digitalWrite(S1, LOW); // Set Selection Pins
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, LOW);
+
+  analog1 = getValue(0);
+
+  analog2 = getValue(2); // Read the Common Output from HEF4051
   
-  analogValue = analogRead(AnalogInput); // Read the Common Output from HEF4051
   
-  digitalWrite(S1, LOW); // Set Selection Pins
-  digitalWrite(S2, HIGH);
-  digitalWrite(S3, LOW);
-  
-  analog2 = analogRead(AnalogInput); // Read the Common Output from HEF4051
-  
-  Serial.print(analogValue); // Print out the Value
+  Serial.print(analog1); // Print out the Value
   Serial.print("    |    ");
   Serial.print(analog2);
   Serial.println();
+  
+}
+
+int getValue( int channel) {
+  // the following sets the selector pins HIGH and LOW to match the binary value of channel
+    for(int bit = 0; bit < 3; bit++){
+
+    int pin = select[bit]; // the pin wired to the multiplexer select bit
+    int isBitSet = bitRead(channel, bit); // true if given bit set in channel
+    digitalWrite(pin, isBitSet);
+//    Serial.print(isBitSet); //for debug
+
+  }
+//  Serial.println(); // for debug
+  return analogRead(analogInput);
 }
--- multiplex_oscilloscope/multiplex_oscilloscope.ino
+++ multiplex_oscilloscope/multiplex_oscilloscope.ino
@@ -1,50 +1,47 @@
-int firstSensor = 0;
-int secondSensor = 0;
+#define analogInput 0 // Common Output (Z)
 
-
-// array of pins used to select 1 of 8 inputs on multiplexer
+int analog1 = 0; // Analog Value to read
+int analog2 = 0;
 
 const int select[] = {2,3,4}; // array of the pins connected to the 4051 input
-const int analogPin = 0; // the analog pin connected to the multiplexer
+
 int inByte = 0;
 
 void setup() {
-
+  
+  Serial.begin(9600); // Setup Serial Port
+  
   for(int bit = 0; bit < 3; bit++){
     pinMode(select[bit], OUTPUT); // set the three select pins to output
   }
-
-  Serial.begin(9600);
+  
   contactProcessing();
 
 }
 
 void loop () {
-
-  // print the values for each channel once per second
   
+    analog1 = getValue(0);
+
+    analog2 = getValue(2); // Read the Common Output from HEF4051
+    
+    
+    Serial.print(analog1); // Print out the Value
+    Serial.print("    |    ");
+    Serial.print(analog2);
+    Serial.println();
+
+/*  
   if (Serial.available() > 0){
-    firstSensor = getValue(2);
-    secondSensor = getValue(3);
-    Serial.write(firstSensor);
-    Serial.write(secondSensor);
-  }
-
-/*
-  for(int channel = 0; channel < 8; channel++) {
-
-    int value = getValue(channel);
-    Serial.print("Ch ");
-    Serial.print(channel);
-    Serial.print(" = ");
-    Serial.print(value);
-    Serial.print("    |   ");
+    
+    Serial.write(analog1);
+    Serial.write(analog2);
+    
   }
   
-  Serial.println();
   */
 
-  delay (50);
+  delay (10);
 }
 
 
@@ -63,6 +60,6 @@ int getValue( int channel) {
 void contactProcessing() {
   while(Serial.available() <= 0){
     Serial.print('A');
-    delay(300);
+    delay(30);
   }
 }
