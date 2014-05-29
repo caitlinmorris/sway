f24f54428034a33a03f86675a5fe215e62f7f56c
caitlinmorris <caitlinannemorris@gmail.com>
2014-05-29 13:02:15 +0200

    changing packet format to be [ ] per multiplexer instead of individual sensor values, also added calibration phase

--- arduino_files/release_BOARD_A/release_BOARD_A.ino
+++ arduino_files/release_BOARD_A/release_BOARD_A.ino
@@ -1,10 +1,10 @@
 /* caitlin morris, may 2014
  adapted from examples in the xbee-arduino library (andrew rapp) and "making things talk"
  
-RELEASE FILE USING XBEE COMMUNICATION
-
-BOARD A (blocks A1 - C4) : 6 multiplexers
-multi_5 : 7 sensors
+ RELEASE FILE USING XBEE COMMUNICATION
+ 
+ BOARD A (blocks A1 - C4) : 6 multiplexers
+ multi_5 : 7 sensors
  
  */
 
@@ -17,18 +17,25 @@ int analogIn = 0; // stores analog value
 int digitalPin = 0; // digital pin to switch high or low
 int analogOut = A0; // analog output pin; will change in getValue switch case
 
-const int smoothSampleSize = 8; // values to sample for smoothing
+const int smoothSampleSize = 10; // values to sample for smoothing
+
+int calibrationValue [numMultiplexers][numChannels]; // store incoming sensor values during calibration phase
+int sensorMin [numMultiplexers][numChannels]; // minimum sensor value for calibration
+int sensorMax [numMultiplexers][numChannels];   // maximum sensor value for calibration
 
 XBee xbee = XBee();
 
 int multiplexers [numMultiplexers][numChannels];
-//uint8_t payload[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // numChannels + 1 (usually 9 total)
-uint8_t payload[] = { 0, 0, 0, 0, 0 }; // smaller payload for testing
+
+uint8_t payload[] = { 0, 0, 0, 0, 0, 0, 0 }; // numMultiplexers + 1
 
 int smoothIndex [numMultiplexers][numChannels];
 int smoothTotal [numMultiplexers][numChannels];
 int smoothAvg [numMultiplexers][numChannels];
 int readings [numMultiplexers][numChannels][smoothSampleSize];
+
+int displacement [numMultiplexers][numChannels];
+int displacementSum [numMultiplexers];
 
 const int multi_0[] = {
   13,12,11}; // array of the pins connected to the 4051 input
@@ -42,12 +49,12 @@ const int multi_4[] = {
   22,24,26};
 const int multi_5[] = {
   28,30,32};
-  
+
 // SH + SL Address of receiving XBee
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40a2684e);
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
-  
+
 void setup() {
 
   Serial.begin(9600); // Setup Serial Port
@@ -61,14 +68,39 @@ void setup() {
     pinMode(multi_4[bit], OUTPUT);
     pinMode(multi_5[bit], OUTPUT);
   }
-  
+
   for(int i=0; i < numMultiplexers; i++){
     for(int j=0; j < numChannels; j++){
       for(int k=0; k < smoothSampleSize; k++){
         readings[i][j][k] = 0;
       }
+      sensorMin[i][j] = 1023;
+      sensorMax[i][j] = 0;
+      displacement[i][j] = 0;
+    }
+    displacementSum[i] = 0;
+  }
+
+  // calibrate during the first five seconds 
+  while (millis() < 5000) {
+
+    for(int i = 0; i < numMultiplexers; i++){
+      for(int j = 0; j < numChannels; j++){
+        calibrationValue[i][j] = getValue(i,j);
+
+        // record the maximum sensor value
+        if (calibrationValue[i][j] > sensorMax[i][j]) {
+          sensorMax[i][j] = calibrationValue[i][j];
+        }
+
+        // record the minimum sensor value
+        if (calibrationValue[i][j] < sensorMin[i][j]) {
+          sensorMin[i][j] = calibrationValue[i][j];
+        }
+      }
     }
   }
+
 }
 
 void loop () {
@@ -76,30 +108,43 @@ void loop () {
   // send one packet per multiplexer
   for(int i = 0; i < numMultiplexers; i++){
     
+    displacementSum[i] = 0; // reset displacement sum value of each multiplexer
+
     payload[0] = i & 0xff; // start each packet with the i value to indicate which multiplexer index we're in
-    
+
     for(int j = 0; j < numChannels; j++){
-      analogIn = map(getValue(i,j), 200, 550, 0, 100);
-      analogIn = constrain(analogIn, 0, 100);
-      
+
+      analogIn = getValue(i,j);
+
       smoothTotal[i][j] = smoothTotal[i][j] - readings[i][j][smoothIndex[i][j]];
       readings[i][j][smoothIndex[i][j]] = analogIn;
       smoothTotal[i][j] = smoothTotal[i][j] + readings[i][j][smoothIndex[i][j]];
       smoothIndex[i][j] = smoothIndex[i][j] + 1;
-      
+
       if(smoothIndex[i][j] > smoothSampleSize)
         smoothIndex[i][j] = 0;
-        
+
       smoothAvg[i][j] = smoothTotal[i][j] / smoothSampleSize;
+
+      if(smoothAvg[i][j] > sensorMax[i][j]){
+        displacement[i][j] = constrain((smoothAvg[i][j] - sensorMax[i][j]),0,126);
+      }
+      else if(smoothAvg[i][j] < sensorMin[i][j]){
+        displacement[i][j] = constrain((sensorMin[i][j] - smoothAvg[i][j]),0,126);
+      }
+      else displacement[i][j] = 0;
       
-      payload[j+1] = analogIn & 0xff; // append each multiplexed sensor to the packet
+      displacementSum[i] += displacement[i][j]; // add each individual sensor displacement to multiplexer sum
     }
-    xbee.send(zbTx);
-//    delay(10);
+    
+    payload[i+1] = displacementSum[i] & 0xff; // append each multiplexed sensor to the packet
+    //    delay(10);
   }    
   
+  xbee.send(zbTx);
+
   // currently leaving out response packet steps!
-  
+
   delay(30);
 }
 
@@ -110,7 +155,7 @@ int getValue( int multiplexer, int channel) {
     for(int bit = 0; bit < 3; bit++){
 
     switch (multiplexer) {
-      case 0:
+    case 0:
       digitalPin = multi_0[bit];
       analogOut = A0;
       break;
@@ -144,3 +189,5 @@ int getValue( int multiplexer, int channel) {
   }
   return analogRead(analogOut);
 }
+
+
